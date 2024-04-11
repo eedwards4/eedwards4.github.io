71eb8cebfb1df5f316631e84cc4555389a77434e
Ethan Edwards <evanevansonbusiness@gmail.com>
2024-04-10 22:26:56 -0700

    > Changes: >> Reworking home page (in progress) >> Make the site one-page (in progress) --------------------------------------- > Plan: >> Homepage >>> All one page >>> Still working on it

--- js/hacked.js
+++ js/hacked.js
@@ -2,7 +2,7 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 
-document.querySelector("h3").onmouseover = event => {
+document.getElementById("hack").onmouseover = event => {
     let iteration = 0;
 
     clearInterval(interval);
